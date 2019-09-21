779dbf4ef7f680969902d10ed60d623d6edaba29
jaywcjlove <398188662@qq.com>
2019-09-21 23:54:29 +0800

    feat(Button): Add size props.

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,3 +1,132 @@
-import Button from './Button';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, ActivityIndicator, TouchableOpacityProps } from 'react-native';
+import { color, colors } from '../utils';
 
-export default Button;
+export interface ButtonProps extends TouchableOpacityProps {
+  color?: string;
+  /**
+   * 设置禁用
+   */
+  disabled?: boolean;
+  /**
+   * 加载状态
+   */
+  loading?: boolean;
+  /**
+   * 按钮类型，可选值为
+   */
+  type?: 'primary' | 'warning';
+  /**
+   * 按钮尺寸
+   */
+  size?: 'small' | 'default' | 'large';
+  /**
+   * 设置按钮圆角
+   */
+  rounded?: boolean | number;
+  /**
+   * 设置是否显示边框
+   */
+  bordered?: boolean;
+}
+
+export default class ButtonView extends React.Component<ButtonProps> {
+  static defaultProps: ButtonProps = {
+    activeOpacity: .5,
+    rounded: 5,
+    bordered: true,
+    size: 'default',
+  };
+  render() {
+    const { children, style, rounded, bordered, color: buttonColor, type, size, disabled, loading, ...restProps } = this.props;
+    let backgroundColor, textColor, borderColor, borderWidth, borderRadius;
+    if (type === 'warning') {
+      backgroundColor = color(colors.red).rgb().string();
+      textColor = color(colors.white).rgb().string();
+    }
+    if (type === 'primary') {
+      backgroundColor = color(colors.green).rgb().string();
+      textColor = color(colors.white).rgb().string();
+    }
+    if (!type) {
+      borderColor = color(colors.black).alpha(0.32).rgb().string();
+      borderWidth = 1;
+    }
+    if (disabled) {
+      textColor = color(textColor).alpha(0.3).rgb().string();
+    }
+    if (buttonColor) {
+      backgroundColor = color(buttonColor).rgb().string();
+      textColor = color(buttonColor).isLight() ? color(buttonColor).darken(0.9).string() : color(buttonColor).lighten(0.9).string();
+    }
+    if (!bordered) {
+      borderWidth = 0;
+    }
+    if (rounded && typeof rounded === 'number') {
+      borderRadius = rounded;
+    }
+    const buttonStyle = { backgroundColor, borderColor, borderWidth, borderRadius };
+    const textStyle = { color: textColor };
+    let sizeStyle = {};
+    if (size && styles[size]) {
+      sizeStyle = styles[size];
+    }
+    return (
+      <TouchableOpacity
+        style={[styles.button, styles.content, buttonStyle, style]}
+        disabled={disabled}
+        {...restProps}
+      >
+        {loading && (
+          <ActivityIndicator
+            size={16}
+            color={textColor}
+            style={styles.icon}
+          />
+        )}
+        {React.isValidElement(children) ? React.cloneElement(children, {
+          style: [textStyle, styles.label]
+        }) : (
+          <Text style={[sizeStyle, textStyle, styles.label]}>{children}</Text>
+        )}
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 3,
+    borderStyle: 'solid',
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 16,
+    marginLeft: 12,
+    marginRight: -4,
+  },
+  label: {
+    textAlign: 'center',
+    letterSpacing: 1,
+  },
+  small: {
+    marginVertical: 6,
+    marginHorizontal: 12,
+    fontSize: 14,
+  },
+  default: {
+    marginVertical: 8,
+    marginHorizontal: 14,
+    fontSize: 16,
+  },
+  large: {
+    marginVertical: 10,
+    marginHorizontal: 16,
+    fontSize: 18,
+  }
+});
