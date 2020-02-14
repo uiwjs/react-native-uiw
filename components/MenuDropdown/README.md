378d2d252780118c90a425a396b42a17403e0db5
xingyuefeng <15262870437@163.com>
2020-02-14 18:06:16 +0800

    feat(MenuDropdown): Complete components

--- components/MenuDropdown/README.md
+++ components/MenuDropdown/README.md
@@ -4,3 +4,42 @@ MenuDropdown
 菜单下拉按钮
 
 ### 基础示例
+
+<!--DemoStart--> 
+```jsx
+import React, { Component } from 'react';
+import { Text  } from 'react-native';
+
+export default class MenuDropdownView extends Component{
+
+  render() {
+    return (
+    <MenuDropdown title="菜单">
+      <MenuDropdown.Item><Text>首页</Text></MenuDropdown.Item>
+      <MenuDropdown.Item><Text>个人中心</Text></MenuDropdown.Item>
+      <MenuDropdown.Item><Text>列表</Text></MenuDropdown.Item>
+    </MenuDropdown>
+    );
+  }
+}
+
+
+```
+<!--End-->
+
+
+## props
+
+组件继承 [`TouchableOpacity`](https://facebook.github.io/react-native/docs/touchableopacity#docsNav)
+
+| 参数 | 说明 | 类型 | 默认值 |
+|------|------|-----|------|
+| `color` | 设置按钮颜色 | String | - |
+| `title` | 按钮标题 | String | - |
+| `textStyle` | 如果按钮内是文本，此样式可以设置文本样式 | `TextProps.Style` | - |
+| `disabled` | 设置禁用 | Boolean | - |
+| `loading` | 加载状态 | Boolean | - |
+| `rounded` | 设置圆角 | Boolean/Number | `5` |
+| `bordered` | 是否有边框 | Boolean | `true` |
+| `size` | 按钮尺寸 | `small`, `default`, `large` | `default` |
+| `type` | 按钮类型，可选值为 | `primary`, `success`, `warning`, `danger`, `light`, `dark` | - |
--- components/MenuDropdown/index.tsx
+++ components/MenuDropdown/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native'
+import React, { RefObject } from 'react';
+import { View, StyleSheet, Text, Animated, findNodeHandle, UIManager, ViewProps } from 'react-native'
 import Item from './item'
 import { ButtonProps } from '../Button'
 import { IconsName, IconsProps } from '../Icon'
@@ -13,6 +13,7 @@ export interface MenuDropdownProps extends ButtonProps {
 interface IState {
   btnIcon: IconsName,
   visibleMenu: Boolean,
+  listHeightValue: Animated.Value,
 }
 
 export default class MenuDropdown extends React.Component<MenuDropdownProps> {
@@ -22,33 +23,68 @@ export default class MenuDropdown extends React.Component<MenuDropdownProps> {
 
   static Item: typeof Item;
 
-  state:IState = {
+
+  state: IState = {
     btnIcon: 'down',
     visibleMenu: false,
+    listHeightValue: new Animated.Value(0)
   }
 
   handleonPress = () => {
-    const { visibleMenu,  } = this.state;
+    const { visibleMenu, } = this.state;
     this.setState({
       visibleMenu: !visibleMenu,
-      btnIcon: visibleMenu ? 'up' : 'down'
+      btnIcon: visibleMenu ? 'down' : 'up'
+    })
+    if (visibleMenu) {
+      this.animateClose()
+    } else {
+      this.animateStart()
+    }
+  }
+
+
+  animateStart = () => {
+    Animated.timing(
+      this.state.listHeightValue,
+      {
+        toValue: 1,
+        duration: 150,
+      }
+    ).start();
+  }
+
+  animateClose = () => {
+    this.setState({
+      listHeightValue: new Animated.Value(0)
     })
   }
+
   render() {
-    const { title, children } = this.props;
-    const { btnIcon, visibleMenu } = this.state;
-    return <View>
-      <Button onPress={this.handleonPress}> 
+    const { title, children, size, ...btnProps } = this.props;
+
+    const { btnIcon, visibleMenu, listHeightValue } = this.state;
+
+    return <View style={styles.menuBox}>
+      <Button {...btnProps} onPress={this.handleonPress} size={size}  >
         <Text>{title}</Text>
         <Icon name={btnIcon} size={17} />
       </Button>
       {
-        visibleMenu && <View style={styles.list}>
-        {children}
-      </View>
+        visibleMenu &&
+        <Animated.View
+          style={[styles.list, {
+            opacity: listHeightValue,
+            top: size === 'large' ? 35 : (size === 'small' ? 21 : 30)
+          }]}
+
+        >
+            {children}
+
+        </Animated.View>
       }
     </View>
-  } 
+  }
 }
 
 MenuDropdown.Item = Item
@@ -56,11 +92,19 @@ MenuDropdown.Item = Item
 
 
 const styles = StyleSheet.create({
+  menuBox: {
+
+  },
   list: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 100,
     marginTop: 10,
     borderColor: '#ddd',
     borderWidth: 1,
-    borderRadius: 3,
-    shadowColor: '#000',
+    borderRadius: 1,
+    backgroundColor: '#fff',
   }
 });
