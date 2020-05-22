3a1ea32c6b66291544a911118fc344f1e12efe19
jaywcjlove <398188662@qq.com>
2020-05-22 17:30:39 +0800

    feat: Add usePrevious.

---
+++ components/utils/hooks.ts
@@ -0,0 +1,22 @@
+import {useRef, useEffect} from 'react';
+
+/**
+ * 获取上一轮的 props 或 state
+ * How to get the previous props or state?
+ * https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
+ * @example
+ * ```js
+ * function Counter() {
+ *   const [count, setCount] = useState(0);
+ *   const prevCount = usePrevious(count);
+ *   return <h1>Now: {count}, before: {prevCount}</h1>;
+ * }
+ * ```
+ */
+export function usePrevious<T>(value: T) {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
