8967407c5dc5d85bf4424c61da5691afcd7d2f20
jaywcjlove <398188662@qq.com>
2021-08-03 10:37:14 +0800

    website: update team page.

--- website/src/pages/team/index.tsx
+++ website/src/pages/team/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Footer from '../../component/Footer';
 import { DefaultProps } from '../../';
 import styles from './index.module.less';
 
 export default function Team(props: DefaultProps) {
   return (
     <div className={styles.warpper}>
-      团队
-      <p>
-        <Link to="/components/about">查看组件文档</Link>
-      </p>
+      <header className={styles.header}>
+        <div>
+          <h2>UIW Mobile RN</h2>
+          <p>
+            一个基于 React Native 的 UI 组件库
+          </p>
+        </div>
+      </header>
+      <Footer />
     </div>
   );
 }
