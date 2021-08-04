93f3abda9b6da721718d2b937918b7895d939b57
jaywcjlove <398188662@qq.com>
2021-08-04 11:59:09 +0800

    website(team): Update page.

--- website/src/routes/router.tsx
+++ website/src/routes/router.tsx
@@ -2,11 +2,12 @@ import { Loader } from 'uiw';
 import dynamic from 'react-dynamic-loadable';
 import { store } from '../models';
 
-
 function Loading() {
-  return <div style={{ textAlign: 'center', padding: '50px 0 50px 0'}}>
-    <Loader color="#333" tip="页面加载中..." />
-  </div>
+  return (
+    <div style={{ textAlign: 'center', padding: '50px 0 50px 0'}}>
+      <Loader color="#333" tip="页面加载中..." />
+    </div>
+  )
 }
 
 // wrapper of dynamic
