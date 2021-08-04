3846def466bc5c13b70041bd19ae21343a48482d
WX <42308734@qq.com>
2021-08-04 15:54:21 +0800

    fix:SearchBar组件增加删除功能

--- components/SearchBar/svg.js
+++
@@ -1,3 +0,0 @@
-export const down = `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="8" viewBox="0 0 13 8">
-<polyline fill="none" stroke="#C7C7CC" stroke-width="2" points="365 203 370.45 208.45 365 213.9" transform="rotate(90 289.725 -74.55)"/>
-</svg>`;
