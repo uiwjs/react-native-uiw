22e4497247bfaceb703481c04bb1fa811886b22b
huqiaoli <huqiaoli@jz-sh.cn>
2021-08-16 23:29:07 +0800

    fix:添加动画

--- example/examples/src/routes/Progress/index.tsx
+++ example/examples/src/routes/Progress/index.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {View, Text} from 'react-native';
 import {Progress, Button, Spacing} from '@uiw/react-native';
-import Layout from '../../Layout';
+import Layout, {Container} from '../../Layout';
+
+const {Header, Body, Card} = Layout;
 
 const ProgressDemo = (props: any) => {
-  const {Header, Card} = Layout;
   const {route} = props;
   const description = route.params.description;
   const title = route.params.title;
@@ -20,22 +21,26 @@ const ProgressDemo = (props: any) => {
   }
 
   return (
-    <SafeAreaView>
-      <Progress progress={30} position="fixed"/>
+    <Container>
+      <Progress progress={30}/>
       <Header title={title} description={description} />
-      <Card title="基础实例" style={{margin:10}}>
-        <Progress progressColor="red" progress={40}/>
-        <Spacing />
-        <Button onPress={onPress}>(+-)10</Button>
-        <Spacing />
-        <Progress progress={val} progressColor='purple'/>
-        <Text style={{ color: 'red', fontSize: 12}}>{val}%</Text>
-        <Spacing />
-        <Progress progressColor="orange" progress={60}/>
-        <Spacing />
-        <Progress progressColor="yellow" progress={80}/>
-      </Card>
-    </SafeAreaView>
+      <Body>
+        <Card title="基础实例" style={{margin:10}}>
+          <Progress progressColor="red" progress={40}/>
+          <Spacing />
+          <Button onPress={onPress}>(+-)10</Button>
+          <Spacing />
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+            <Progress progress={val} progressColor='purple'/>
+            <Text style={{fontSize: 12, width: 40, textAlign: 'right'}}>{val}%</Text>
+          </View>
+          <Spacing />
+          <Progress progressColor="orange" progress={60}/>
+          <Spacing />
+          <Progress progressColor="yellow" progress={80}/>
+        </Card>
+      </Body>
+    </Container>
   );
 };
 
--- packages/core/src/Progress/index.tsx
+++ packages/core/src/Progress/index.tsx
@@ -1,52 +1,77 @@
-import React, {useState, useEffect} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {
   Animated,
   View,
   StyleSheet,
-  ViewProps
+  ViewProps,
+  LayoutChangeEvent
 } from 'react-native'
 
 type PositionType = 'fixed' | 'relative'
 
 // 组件的属性定义
-export interface PropsDefineProps  extends ViewProps {
+export interface ProgressProps  extends ViewProps {
   /** 当前进度百分比， 0 - 100, 默认0 */
   progress?: number;
-  /** 颜色， 默认10 */
+  /** 颜色 */
   progressColor? : string
   /** 位置 */
   position?: PositionType;
+  /** 动画持续的时间 */
+  animation?: { duration?: number; } | boolean;
 }
 
-export default (props: PropsDefineProps) => {
+export default (props: ProgressProps) => {
   const {
     style,
     progress = 0,
     progressColor = '#108ee9',
-    position = 'fixed'
+    position,
+    animation = { duration: 500 },
   } = props;
 
-  const [val, setVal] = useState<number>(0);
+  const progWidth = useRef<any>(new Animated.Value(0)).current;
+  const [wrapWidth, setWrapWidth] = useState<number>(0);
 
   useEffect(() => {
-    let num: number;
-    if (progress >= 100) {
-      num = 100;
-    } else if (progress <= 0) {
-      num = 0;
-    } else {
-      num = progress;
-    }
-    setVal(num);
-  }, [progress])
+    startAnimation();
+  }, [wrapWidth, progress])
 
+  const startAnimation = () => {
+    Animated.timing(progWidth, {
+      toValue: getWidth(),
+      duration: typeof animation !== 'boolean' ? animation.duration : 1000,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const onLayout = (e: LayoutChangeEvent) => {
+    setWrapWidth(e.nativeEvent.layout.width);
+  }
+
+  const getWidth = (percent: number = progress) => {
+    return wrapWidth * (normalPercent(percent) / 100)
+  }
+
+  const normalPercent = (percent?: number) => {
+    let widthPercent: any = 0
+    if (percent !== undefined && percent > 0) {
+      widthPercent = percent > 100 ? 100 : percent
+    }
+    return widthPercent
+  }
+  
   return (
     <View style={[styles.container, style]}>
-      <View style={[
+      <View onLayout={onLayout} style={[
         styles.pre, 
         position === 'fixed' ? { position: 'absolute', top: 0 } : {},
         { borderColor: progressColor }]}>
-        <Animated.View style={[styles.preOisn, { width: `${val}%`, backgroundColor: progressColor }]}></Animated.View>
+        <Animated.View 
+          style={[styles.preOisn, {
+            width: progWidth,
+            backgroundColor: progressColor
+          }]}></Animated.View>
       </View>
     </View>
   )
@@ -54,15 +79,13 @@ export default (props: PropsDefineProps) => {
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     flex: 1,
   },
   pre: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     borderWidth: 1,
-    width: '100%',
     height: 4,
+    width: '100%',
     borderRadius: 20,
     marginBottom: 0,
     marginTop: 0,
@@ -71,8 +94,7 @@ const styles = StyleSheet.create({
   preOisn: {
     position: 'absolute',
     height: 4,
-    borderBottomLeftRadius: 2000,
-    borderTopLeftRadius: 2000,
-    zIndex: 8
+    left: 0,
+    top: 0,
   },
 });
