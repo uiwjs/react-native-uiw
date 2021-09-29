7127f74f89adacc1a225e608b2cb93da4b35301f
崔兰鑫 <1192065030@qq.com>
2021-09-29 13:54:11 +0800

    fix(Pagination): 优化分页器在安卓上文字显示 (#254)
    * 编写选项卡组件
    
    * 类型名称请调整，添加组件文档在网站上展示
    
    * #162修复报错
    
    * fix:#162修复报错
    
    * SpeedDial 悬浮标记
    
    * 优化类型
    
    * feat(ActionSheet): Add new component.
    
    * fix: ActionSheet 实例优化，组件优化
    
    * feat(SearchInputBar): Add new component.
    
    * feat(Pagination): Add new component.
    
    * fix(Tooltip): 修复cloud弹出元素位置问题
    
    * doc(Drawer): Update README.md
    
    * doc: Update README.md
    
    * fix(Rating): Rating 评分组件 使用繁琐(#244)
    
    * fix(Rating): Rating 评分组件 使用繁琐(#244)
    
    * fix(Slider): 滑块输入条 thumbSize属性没有效果, 滑块输入条 thumbSize 设置很大时然后使用 shownThumb=false 效果如下 (#234 # 233)
    
    * fix(Slider): 滑块输入条 thumbSize属性没有效果, 滑块输入条 thumbSize 设置很大时然后使用 shownThumb=false 输入条拉不满  (#234 # 233)
    
    * fix(Slider): Slider 滑块 输入条 vertical={true} 会出现一直 render (#237)
    
    * feat: Upgrade react-native to v0.65.1
    
    * fix(Pagination): android文字被挤压问题
    
    * fix(Pagination): 优化分页器在安卓上文字现实
    
    * fix(Pagination): 优化分页器在安卓上文字显示
    
    * fix(Pagination): 优化分页器在安卓上文字显示

--- packages/core/src/Pagination/DirText.tsx
+++ packages/core/src/Pagination/DirText.tsx
@@ -31,15 +31,11 @@ const DirText = (props: DirTextProps) => {
   }, [disabled]);
 
   return (
-    <View
-      style={[
-        styles.containerStyle,
-        { minWidth: containerSize[size], height: containerSize[size], paddingHorizontal: icon ? 0 : 5 },
-      ]}
-    >
+    <View style={[styles.containerStyle, { minWidth: containerSize[size], paddingHorizontal: icon ? 0 : 5 }]}>
       <Button
         bordered={false}
         disabled={disabled}
+        size={props.size}
         onPress={() => {
           onPageChange(direction === 'left' ? 'prev' : 'next');
         }}
@@ -47,7 +43,7 @@ const DirText = (props: DirTextProps) => {
         {icon ? (
           <Icon name={direction} size={contentSize[size]} color={disabledStyle} />
         ) : (
-          <Text style={{ fontSize: contentSize[size], color: disabledStyle }}>{dirText}</Text>
+          <Text style={{ color: disabledStyle }}>{dirText}</Text>
         )}
       </Button>
     </View>
