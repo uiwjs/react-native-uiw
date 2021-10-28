ca376f6da2aaaf219a5ee7d217b1b53ba09f7ecd
杨楠 <yangnan@nihaosi.com>
2021-10-28 13:12:29 +0800

    style:Update local code

--- packages/core/src/Ellipsis/index.tsx
+++ packages/core/src/Ellipsis/index.tsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, TextProps, Dimensions, TouchableOpacity } from 'react-native';
-import Icon from '../Icon';
-import { right } from './svg';
-import MaskLayer from '../MaskLayer';
+import { Text, TextProps } from 'react-native';
 
-let MainHeight = Dimensions.get('window').height;
 export interface EllipsisProps extends TextProps {
   children?: React.ReactNode;
   placeholder?: string;
@@ -13,39 +9,11 @@ export interface EllipsisProps extends TextProps {
 }
 
 export default function Ellipsis({ maxLen, children, placeholder, ...props }: EllipsisProps) {
-  const [modalVisible, setModalVisible] = useState(false);
-  // let content1 = Number(children.length);
-
   let content = children;
   if (maxLen && content && typeof content === 'string') {
     content = content.length > maxLen ? content.substr(0, maxLen) + placeholder : content;
   }
-
-  return (
-    <>
-      <MaskLayer visible={modalVisible} onDismiss={() => setModalVisible(false)}>
-        <View
-          style={{
-            backgroundColor: '#fff',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: MainHeight * 0.2,
-            padding: 20,
-          }}
-        >
-          <Text style={{ fontSize: 17 }}>{children}</Text>
-        </View>
-      </MaskLayer>
-
-      <TouchableOpacity
-        style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text {...props}>{content}</Text>
-        <Icon name="right" size={14} />
-      </TouchableOpacity>
-    </>
-  );
+  return <Text {...props}>{content}</Text>;
 }
 
 Ellipsis.propTypes = {
