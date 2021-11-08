53784a729c7f644235a8accd2b4abe253d79b3bb
杨楠 <yangnan@nihaosi.com>
2021-11-08 14:40:42 +0800

    feat(Timeline):优化自定义图标 & 文档更新

--- example/examples/src/routes/Timeline/index.tsx
+++ example/examples/src/routes/Timeline/index.tsx
@@ -20,11 +20,6 @@ export default (props: StepsViewProps) => {
     {
       title: '组件化',
       desc: '构建管理自身状态的封装组件，然后对其组合以构成复杂的 UI。',
-      icon: {
-        name: 'warning',
-        size: 20,
-        color: 'blue',
-      },
     },
     {
       title: '一次学习，随处编写',
@@ -39,27 +34,27 @@ export default (props: StepsViewProps) => {
       title: '声明式声明式',
       tips: '2021-08-07 12:00:00',
       desc: 'React 使创建交互式',
-      icon: {
-        color: 'red',
-      },
+      icon: <Icon name="smile" fill="red" size={18} />,
     },
     {
       title: '组件化',
       tips: '2021-08-08 12:00:00',
       desc: '构建管理自身状态。',
-      icon: {
-        color: 'green',
-      },
+      size: 20,
+      color: 'blue',
+      icon: 'qq',
     },
     {
       title: '随处编写',
       tips: '2021-08-09 12:00:00',
       desc: '服务器渲染。',
+      color: 'green',
     },
     {
       title: '一次学习，随处编写',
       tips: '2021-08-10 12:00:00',
       desc: '开发新功能。',
+      size: 30,
     },
   ];
 
@@ -70,7 +65,7 @@ export default (props: StepsViewProps) => {
         description={route.params.description}
       />
       <Body>
-        <Card title={`基础用法 & 自定义图标`}>
+        <Card title="基础用法">
           <WingBlank>
             <Timeline style={{backgroundColor: '#fff'}} items={item} />
           </WingBlank>
@@ -84,7 +79,7 @@ export default (props: StepsViewProps) => {
             />
           </WingBlank>
         </Card>
-        <Card title="交替展现">
+        <Card title={`交替展现 & 自定义图标`}>
           <WingBlank>
             <Timeline
               style={{backgroundColor: '#fff'}}
