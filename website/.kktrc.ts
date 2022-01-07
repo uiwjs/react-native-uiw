004323623db81fe6072e8ad9a44a6a3f73365c8b
jaywcjlove <398188662@qq.com>
2022-01-07 21:41:31 +0800

    website: fix the development preview issue. #365

--- website/.kktrc.ts
+++ website/.kktrc.ts
@@ -21,6 +21,8 @@ export default (conf: Configuration, env: 'development' | 'production', options:
       VERSION: JSON.stringify(pkg.version),
     }),
   );
-  conf.output = { ...conf.output, publicPath: './' };
+  if (env === 'production') {
+    conf.output = { ...conf.output, publicPath: './' };
+  }
   return conf;
 };
