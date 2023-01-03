1d3850e13a23b00d88fb1985372ebfebe1cec301
ff <ff@ffdeMac-Pro.local>
2023-01-03 17:47:15 +0800

    test: 添加Button测试

---
+++ test-ci/src/utils.ts
@@ -0,0 +1,24 @@
+const colorRgb = (color: string): string => {
+  var sColor = color.toLowerCase();
+  //十六进制颜色值的正则表达式
+  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+  // 如果是16进制颜色
+  if (sColor && reg.test(sColor)) {
+    if (sColor.length === 4) {
+      var sColorNew = '#';
+      for (var i = 1; i < 4; i += 1) {
+        sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+      }
+      sColor = sColorNew;
+    }
+    //处理六位的颜色值
+    var sColorChange = [];
+    for (var i = 1; i < 7; i += 2) {
+      sColorChange.push(parseInt('0x' + sColor.slice(i, i + 2)));
+    }
+    return 'rgb(' + sColorChange.join(', ') + ')';
+  }
+  return sColor;
+};
+
+export { colorRgb };
