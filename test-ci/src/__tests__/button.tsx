49987c59aeab9ace26557e60173f6e1620f9a65d
SunLxy <1011771396@qq.com>
2023-01-03 10:23:50 +0800

    chore:修复react-native依赖包问题

--- test-ci/src/__tests__/button.tsx
+++ test-ci/src/__tests__/button.tsx
@@ -10,7 +10,17 @@ import renderer from 'react-test-renderer';
 
 it('Button', () => {
   // const component = TestRenderer.create(<Radio value="1">Radio</Radio>);
-  const component = renderer.create(<Button type="primary">按钮</Button>);
+  const component = renderer.create(
+    <Button type="primary" rounded disabled loading={true} size="large" bordered color="red">
+      按钮
+    </Button>,
+  );
   expect(component.root.props.type).toBe('primary');
+  expect(component.root.props.color).toBe('red');
+  expect(component.root.props.rounded).toBeTruthy();
+  expect(component.root.props.disabled).toBeTruthy();
+  expect(component.root.props.loading).toBeTruthy();
+  expect(component.root.props.bordered).toBeTruthy();
+  expect(component.root.props.size).toBe('large');
   expect(component.root.props.children).toBe('按钮');
 });
