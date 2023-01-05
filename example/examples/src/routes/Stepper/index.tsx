b093d2135a3487dacd1626b3008cf950cddf137d
shenzhiqing <shenzhiqing@nihaosi.com>
2023-01-05 17:49:48 +0800

    fix(Stepper): 修改Stepper实例

--- example/examples/src/routes/Stepper/index.tsx
+++ example/examples/src/routes/Stepper/index.tsx
@@ -16,10 +16,7 @@ export interface IndexState {
   value1: number;
   value2: number;
 }
-export default class StepperExample extends React.Component<
-  IndexProps,
-  IndexState
-> {
+export default class StepperExample extends React.Component<IndexProps, IndexState> {
   state = {
     value: 0,
     value1: 2,
@@ -38,97 +35,80 @@ export default class StepperExample extends React.Component<
         <Layout>
           <Header title={title} description={description} />
           <Body>
-            <List flat={false} extra="test">
-              <List.Item
-                extra={
-                  <Stepper
-                    value={this.state.value}
-                    onChange={value => {
-                      this.setState({value});
-                    }}
-                  />
-                }>
-                基本使用：
-              </List.Item>
-              <List.Item
-                extra={
-                  <Stepper
-                    size="small"
-                    value={this.state.value}
-                    onChange={value => {
-                      this.setState({value});
-                    }}
-                  />
-                }>
-                尺寸控制(size: small | default | large)：
-              </List.Item>
-              <List.Item
-                extra={
-                  <Stepper
-                    disabledLongPress={true}
-                    value={this.state.value2}
-                    onChange={value2 => {
-                      this.setState({value2});
-                    }}
-                  />
-                }>
-                按钮开启长按(disabledLongPress: boolean)：
-              </List.Item>
-              <List.Item
-                extra={
-                  <Stepper
-                    width={120}
-                    value={this.state.value2}
-                    onChange={value2 => {
-                      this.setState({value2});
-                    }}
-                  />
-                }>
-                自定义宽度(width: number)：
-              </List.Item>
-              <List.Item
-                extra={
-                  <Stepper
-                    value={this.state.value}
-                    color={{
-                      color: '#ccc',
-                      borderColor: '#999',
-                      controlColor: 'red',
-                      valueColor: '#000',
-                    }}
-                    onChange={value => {
-                      this.setState({value});
-                    }}
-                  />
-                }>
-                自定义颜色(color: Color)：
-              </List.Item>
-              <List.Item
-                extra={
-                  <Stepper
-                    disabledInput={false}
-                    value={this.state.value1}
-                    onChange={value1 => {
-                      this.setState({value1});
-                    }}
-                  />
-                }>
-                不禁止输入(disabledInput: boolean)：
-              </List.Item>
-              <List.Item
-                extra={
-                  <Stepper
-                    disabled={true}
-                    disabledInput={false}
-                    value={this.state.value1}
-                    onChange={value1 => {
-                      this.setState({value1});
-                    }}
-                  />
-                }>
-                禁止点击(disabled: boolean)：
-              </List.Item>
-            </List>
+            <Card title="基础实例">
+              <Stepper
+                value={this.state.value}
+                onChange={value => {
+                  this.setState({value});
+                }}
+              />
+            </Card>
+
+            <Card title="尺寸控制(size: small | default | large)：">
+              <Stepper
+                size="small"
+                value={this.state.value}
+                onChange={value => {
+                  this.setState({value});
+                }}
+              />
+            </Card>
+
+            <Card title="按钮开启长按(disabledLongPress: boolean)：">
+              <Stepper
+                disabledLongPress={true}
+                value={this.state.value2}
+                onChange={value2 => {
+                  this.setState({value2});
+                }}
+              />
+            </Card>
+
+            <Card title="自定义宽度(width: number)：">
+              <Stepper
+                width={120}
+                value={this.state.value2}
+                onChange={value2 => {
+                  this.setState({value2});
+                }}
+              />
+            </Card>
+
+            <Card title="自定义颜色(color: Color)：">
+              <Stepper
+                value={this.state.value}
+                color={{
+                  color: '#ccc',
+                  borderColor: '#999',
+                  controlColor: 'red',
+                  valueColor: '#000',
+                }}
+                onChange={value => {
+                  this.setState({value});
+                }}
+              />
+            </Card>
+
+            <Card title="不禁止输入(disabledInput: boolean)：">
+              <Stepper
+                disabledInput={false}
+                value={this.state.value1}
+                onChange={value1 => {
+                  this.setState({value1});
+                }}
+              />
+            </Card>
+
+            <Card title="禁止点击(disabled: boolean)：">
+              <Stepper
+                disabled={true}
+                disabledInput={false}
+                value={this.state.value1}
+                onChange={value1 => {
+                  this.setState({value1});
+                }}
+              />
+            </Card>
           </Body>
           <Footer />
         </Layout>
