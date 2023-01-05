9641652d725127a778fbe6ff95bffe7e51942350
huqiaoli <huqiaoli@jz-sh.cn>
2023-01-05 18:03:53 +0800

    fix(imageViewer):添加自定义多张图片查看

--- example/examples/src/routes/ImageViewer/index.tsx
+++ example/examples/src/routes/ImageViewer/index.tsx
@@ -21,11 +21,19 @@ export default class Index extends Component<IndexProps> {
             <Card title="基础使用">
               <ImageViewer />
             </Card>
-            <Card title="自定义图片">
+            <Card title="自定义单张图片">
+              <ImageViewer height={100} width={100} src="https://img11.51tietu.net/pic/2016-071418/20160714181543xyu10ukncwf221991.jpg" />
+            </Card>
+            <Card title="自定义多张图片">
               <ImageViewer
                 height={100}
                 width={100}
-                src="https://img11.51tietu.net/pic/2016-071418/20160714181543xyu10ukncwf221991.jpg"
+                defaultIndex={2}
+                src={[
+                  {url: 'https://wx3.sinaimg.cn/mw690/4718260ely1gt2cg7t5udj23dw1wkhdu.jpg'},
+                  {url: 'https://iknow-pic.cdn.bcebos.com/810a19d8bc3eb135828572d2ab1ea8d3fd1f441d'},
+                  {url: 'https://wx1.sinaimg.cn/mw690/4718260ely1gt2cg5r9zij22yo1o0x6p.jpg'},
+                ]}
               />
             </Card>
           </Body>
