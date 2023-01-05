562f49720dfa5e2c4ff366e7cf2f0d874cd55168
WmW <zhouzheng0646@qq.com>
2023-01-05 13:47:53 +0800

    doc(TextArea): 重构多行输入框TextArea文档 (#410)

--- website/src/pages/components/textarea/index.tsx
+++ website/src/pages/components/textarea/index.tsx
@@ -1,11 +1,6 @@
-import Markdown, { importAll } from '../../../component/Markdown';
+// import Markdown, { importAll } from '../../../component/Markdown';
+import Preview from 'src/component/Preview';
+import md from '@uiw/react-native/lib/TextArea/README.md';
 
-export default class Page extends Markdown {
-  path = '/packages/core/src/TextArea/README.md';
-  getMarkdown = async () => {
-    const md = await import('@uiw/react-native/lib/TextArea/README.md');
-    // 支持 markdown 中，相对于当前 index.tsx 相对路径引入图片资源
-    importAll((require as any).context('./', true, /\.(png|gif|jpg|svg)$/), this.imageFiles);
-    return md.default || md;
-  };
-}
+const DEMO = () => <Preview {...md} />;
+export default DEMO;
