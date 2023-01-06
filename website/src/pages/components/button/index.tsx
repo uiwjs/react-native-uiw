3737f3d513d93110892a13649f83ba3f49c53ac2
ff <ff@ffdeMac-Pro.local>
2023-01-06 13:50:40 +0800

    docs: 修改文档预览

--- website/src/pages/components/button/index.tsx
+++ website/src/pages/components/button/index.tsx
@@ -1,15 +1,5 @@
-// import Markdown, { importAll } from '../../../component/Markdown';
 import Preview from 'src/component/Preview';
 import md from '@uiw/react-native/lib/Button/README.md';
 
 const DEMO = () => <Preview {...md} />;
 export default DEMO;
-// export default class Page extends Markdown {
-//   path = '/packages/core/src/Button/README.md';
-//   getMarkdown = async () => {
-//     const md = await import('@uiw/react-native/lib/Button/README.md');
-//     // 支持 markdown 中，相对于当前 index.tsx 相对路径引入图片资源
-//     importAll((require as any).context('./', true, /\.(png|gif|jpg|svg)$/), this.imageFiles);
-//     return md.default || md;
-//   };
-// }
