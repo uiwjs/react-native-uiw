75d5e927ce0b19ee0479a3cae54b89319841c18a
ff <ff@ffdeMac-Pro.local>
2023-01-06 10:17:14 +0800

    docs: 优化Icon文档

--- website/src/pages/components/icon/index.tsx
+++ website/src/pages/components/icon/index.tsx
@@ -1,13 +1,5 @@
-import svgPaths from '@uiw/icons/fonts/w-icon.json';
-import Markdown, { importAll } from '../../../component/Markdown';
+import Preview from 'src/component/Preview';
+import md from '@uiw/react-native/lib/Icon/README.md';
 
-export default class Page extends Markdown {
-  dependencies = { svgPaths };
-  path = '/packages/core/src/Icon/README.md';
-  getMarkdown = async () => {
-    const md = await import('@uiw/react-native/lib/Icon/README.md');
-    // 支持 markdown 中，相对于当前 index.tsx 相对路径引入图片资源
-    importAll((require as any).context('./', true, /\.(png|gif|jpg|svg)$/), this.imageFiles);
-    return md.default || md;
-  };
-}
+const MD = () => <Preview {...md} />;
+export default MD;
