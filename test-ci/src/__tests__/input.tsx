e5f47f451d78e4627829ba110d595a7688ce3403
yaobin <yaob421123@163.com>
2023-01-09 10:16:45 +0800

    test(Input): add input test (#447)
    * test: add ActionBar component test
    
    * refactor(Input): class component to function component
    
    * fix: 处理冲突
    
    * test(Input): add input test

--- test-ci/src/__tests__/input.tsx
+++ test-ci/src/__tests__/input.tsx
@@ -1,14 +1,35 @@
 import 'react-native';
 import React from 'react';
 import Input from '../lib/Input';
+import Icon from '../lib/Icon';
+import { TextInput, Text } from 'react-native';
 import { render, fireEvent } from '@testing-library/react-native';
 
 describe('input', () => {
-  it('snapshot', () => {
-    const DefaultInput = () => {
-      return <Input testID="input" />;
-    };
-    // const { toJSON } = render(<DefaultInput />);
-    // expect(toJSON()).toMatchSnapshot();
+  it('disabled', () => {
+    const { getByTestId } = render(<Input disabled testID="input" />);
+    const component = getByTestId('input');
+    expect(component.props.editable).toBe(false);
+  });
+
+  it('clear', () => {
+    const { getByText } = render(<Input clear />);
+    expect(getByText('清除')).toBeTruthy();
+  });
+
+  it('renderClear', () => {
+    const { getByText } = render(<Input clear renderClear={<Text>remove</Text>} />);
+    expect(getByText('remove')).toBeTruthy();
+  });
+
+  it('error', () => {
+    const { UNSAFE_getByType } = render(<Input error testID="input" />);
+    const icons = UNSAFE_getByType(Icon);
+    expect(icons.props.name).toBe('circle-close');
+  });
+
+  it('renderError', () => {
+    const { getByText } = render(<Input error renderError={<Text>error text</Text>} />);
+    expect(getByText('error text')).toBeTruthy();
   });
 });
