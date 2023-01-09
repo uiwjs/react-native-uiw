090590d20afd4e6662119105535afca04eb32561
ff <ff@ffdeMac-Pro.local>
2023-01-09 16:22:28 +0800

    docs: 更新文档预览方式

--- website/src/pages/docs/development/components/index.tsx
+++ website/src/pages/docs/development/components/index.tsx
@@ -1,6 +1,7 @@
 import Preview from 'src/component/Preview';
 import md from './README.md';
 
+<<<<<<< HEAD
 const transformImageUri = (url: string) => {
   const reqImage = (require as any).context!('./', true, /\.(png|gif|jpg|svg)$/);
   const urlAddr = reqImage(url);
@@ -14,4 +15,7 @@ const DEMO = () => (
     path="website/src/pages/docs/development/components/README.md"
   />
 );
+=======
+const DEMO = () => <Preview {...md} />;
+>>>>>>> 3905857c (docs: 更新文档预览方式)
 export default DEMO;
