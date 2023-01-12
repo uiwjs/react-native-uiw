479c7a29a854610039d32e0f176ae181c8daef4c
ff <ff@ffdeMac-Pro.local>
2023-01-12 13:35:47 +0800

    fix: 增加文档底部链接

--- website/src/pages/docs/development/document/index.tsx
+++ website/src/pages/docs/development/document/index.tsx
@@ -1,9 +1,5 @@
 import Preview from 'src/component/Preview';
 import md from './README.md';
 
-<<<<<<< HEAD
 const DEMO = () => <Preview {...md} path="website/src/pages/docs/development/document/README.md" />;
-=======
-const DEMO = () => <Preview {...md} />;
->>>>>>> 3905857c (docs: 更新文档预览方式)
 export default DEMO;
