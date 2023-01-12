cb9e909b541350cbc522d2eee71cfd9ed527f7c8
ff <ff@ffdeMac-Pro.local>
2023-01-12 11:03:36 +0800

    fix: 调整底部链接样式

--- website/src/component/Preview/Footer.js
+++ website/src/component/Preview/Footer.js
@@ -6,12 +6,17 @@ export default function (props) {
   return (
     <div style={{ paddingBlock: 20 }}>
       {url && (
-        <a href={url} target="_blank" rel="noopener noreferrer">
+        <a href={url} target="_blank" rel="noopener noreferrer" style={{ color: '#333' }}>
           在Github上编辑本页！
         </a>
       )}
       <Divider type="vertical" />
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/uiwjs/react-native-uiw/issues">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/uiwjs/react-native-uiw/issues"
+        style={{ color: '#333' }}
+      >
         提交bug
       </a>
     </div>
