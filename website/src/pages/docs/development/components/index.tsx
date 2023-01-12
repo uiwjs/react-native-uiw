45505cbd844300ad74ce40497b215754ab047feb
ff <ff@ffdeMac-Pro.local>
2023-01-12 13:51:00 +0800

    fix: 增加文档底部链接

--- website/src/pages/docs/development/components/index.tsx
+++ website/src/pages/docs/development/components/index.tsx
@@ -1,7 +1,6 @@
 import Preview from 'src/component/Preview';
 import md from './README.md';
 
-<<<<<<< HEAD
 const transformImageUri = (url: string) => {
   const reqImage = (require as any).context!('./', true, /\.(png|gif|jpg|svg)$/);
   const urlAddr = reqImage(url);
@@ -15,7 +14,4 @@ const DEMO = () => (
     path="website/src/pages/docs/development/components/README.md"
   />
 );
-=======
-const DEMO = () => <Preview {...md} />;
->>>>>>> 3905857c (docs: 更新文档预览方式)
 export default DEMO;
