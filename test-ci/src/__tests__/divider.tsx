be53cdd0bc354503be7f8dd5f0bb82521375239a
姚宾 <yaob421123@163.com>
2023-02-14 10:30:14 +0800

    test(Divider): fix test

--- test-ci/src/__tests__/divider.tsx
+++ test-ci/src/__tests__/divider.tsx
@@ -1,12 +1,53 @@
-import Divider from '../lib/Divider';
 import renderer from 'react-test-renderer';
 
-it('Divider', () => {
-  const component = renderer.create(
-    <Divider label="这是一条分割线" type='horizontal' gutter={10} orientation='center' />
-  );
-  expect(component.root.props.label).toBe('这是一条分割线');
-  expect(component.root.props.type).toBe('horizontal');
-  expect(component.root.props.gutter).toBe(10);
-  expect(component.root.props.orientation).toBe('center');
+import 'react-native';
+import React from 'react';
+import Divider from '../lib/Divider';
+import { render, fireEvent } from '@testing-library/react-native';
+import { keys, colorRgb, toObject } from '../utils';
+
+const TYPES: keys = {
+  horizontal: 'row',
+  vertical: 'column',
+};
+
+describe('Divider', () => {
+  it('label', () => {
+    const { getByTestId } = render(<Divider label="这是一条分割线" />);
+    const component = getByTestId('RNE__Divider__label');
+    const lable = component.props.children[1];
+    expect(lable).toBe('这是一条分割线');
+  });
+
+  describe.each`
+    type
+    ${'horizontal'}
+    ${'vertical'}
+  `('$type', ({ type }) => {
+    it(`should display type ${type} divider`, () => {
+      const { getByTestId } = render(<Divider type={type} />);
+      const component = getByTestId('RNE__Divider__wrap');
+      const styles = toObject(component.props.style);
+      expect(styles.flexDirection).toBe(TYPES[type]);
+    });
+  });
+
+  it('gutter', () => {
+    const { getByTestId } = render(<Divider gutter={12} />);
+    const component = getByTestId('RNE__Divider__start');
+    const styles = toObject(component.props.style);
+    expect(styles.marginHorizontal).toBe(12);
+  });
+
+  describe.each`
+    orientation
+    ${'left'}
+    ${'right'}
+    ${'center'}
+  `('$orientation', ({ orientation }) => {
+    it(`should display orientation ${orientation} divider`, () => {
+      const { toJSON } = render(<Divider label="这是一条分割线" orientation={orientation} />);
+      expect(toJSON()).toMatchSnapshot();
+    });
+  });
 });
