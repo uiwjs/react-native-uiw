80916ea791cd39bb1e162cbe5837b67501271c32
hy <huyi@163.com>
2023-04-08 15:14:55 +0800

    fix(MenuDropdown):添加菜单下拉按钮明暗主题色

--- example/examples/src/routes/MenuDropdown/index.tsx
+++ example/examples/src/routes/MenuDropdown/index.tsx
@@ -1,38 +1,38 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import {MenuDropdown, Toast} from '@uiw/react-native';
-import Layout, {Container} from '../../Layout';
-import {ComProps} from '../../routes';
+import React, { Component } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { MenuDropdown, Toast, Text } from '@uiw/react-native';
+import Layout, { Container } from '../../Layout';
+import { ComProps } from '../../routes';
 
-const {Header, Body, Card, Footer} = Layout;
+const { Header, Body, Card, Footer } = Layout;
 
-export interface MenuDropdownProps extends ComProps {}
+export interface MenuDropdownProps extends ComProps { }
 
 export default class MenuDropdownView extends Component<MenuDropdownProps> {
   render() {
-    const {route} = this.props;
+    const { route } = this.props;
     const description = route.params.description;
     const title = route.params.title;
     return (
       <Container>
         <Layout>
           <Header title={title} description={description} />
-          <Body>
-            <Card title="基础实例" style={styles.card}>
+          <Body style={{ padding: 15 }}>
+            <Card title="基础实例">
               <MenuDropdown title="菜单">
                 <MenuDropdown.Item>
-                  <Text>首页</Text>
+                  <Text color="text">首页</Text>
                 </MenuDropdown.Item>
                 <MenuDropdown.Item>
-                  <Text>个人中心</Text>
+                  <Text color="text">个人中心</Text>
                 </MenuDropdown.Item>
                 <MenuDropdown.Item>
-                  <Text>列表</Text>
+                  <Text color="text">列表</Text>
                 </MenuDropdown.Item>
                 <MenuDropdown.Item>
                   <TouchableOpacity
                     onPress={() => Toast.info('你点击了选择一', 2, 'info')}>
-                    <Text>选择一</Text>
+                    <Text color="text">选择一</Text>
                   </TouchableOpacity>
                 </MenuDropdown.Item>
               </MenuDropdown>
@@ -45,10 +45,3 @@ export default class MenuDropdownView extends Component<MenuDropdownProps> {
   }
 }
 
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#fff',
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-});
