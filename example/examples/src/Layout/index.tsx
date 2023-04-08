065d34fbdcfee6995f2dfc6017aa6e80ee857250
WX <42308734@qq.com>
2023-04-08 09:14:47 +0800

    fix: example Layout明暗主题色调整

--- example/examples/src/Layout/index.tsx
+++ example/examples/src/Layout/index.tsx
@@ -16,6 +16,12 @@ export interface HeaderProps {
 
 const Header = (props: HeaderProps) => {
   const {children, title, description, style, hasLogo, titleStyle, descriptionStyle} = props;
+  const theme = useTheme<Theme>();
+  const styles = createStyles({
+    backgroundColor: theme.colors.background,
+    shadowColor: theme.colors.border,
+    color: theme.colors.primary_text,
+  });
   return (
     <View style={[styles.header, style]}>
       {title && <Text style={[styles.title, titleStyle]}>{title}</Text>}
@@ -39,8 +45,13 @@ export interface BodyProps extends ScrollViewProps {
 const Body = (props: BodyProps) => {
   const {children, style, isScroll, ...other} = props;
   const theme = useTheme<Theme>();
+  const styles = createStyles({
+    backgroundColor: theme.colors.background,
+    shadowColor: theme.colors.border,
+    color: theme.colors.primary_text,
+  });
   return (
-    <ScrollView style={[style, {backgroundColor: theme.colors.background || '#fff'}]} automaticallyAdjustContentInsets={false} {...other}>
+    <ScrollView style={[styles.body, style]} automaticallyAdjustContentInsets={false} {...other}>
       {children}
     </ScrollView>
   );
@@ -55,6 +66,12 @@ export interface FooterProps {
 
 const Footer = (props: FooterProps) => {
   const {children, copyright, style, isShowCopyRight, ...other} = props;
+  const theme = useTheme<Theme>();
+  const styles = createStyles({
+    backgroundColor: theme.colors.background,
+    shadowColor: theme.colors.border,
+    color: theme.colors.primary_text,
+  });
   return (
     <View style={[styles.footer, style]} {...other}>
       {isShowCopyRight && <Text style={styles.copyright}>{copyright}</Text>}
@@ -75,6 +92,12 @@ export interface CardProps extends ViewProps {
 
 const Card = (props: CardProps) => {
   const {title, titleStyle, bodyStyle, children, style, showTitle, extra, ...other} = props;
+  const theme = useTheme<Theme>();
+  const styles = createStyles({
+    backgroundColor: theme.colors.background,
+    shadowColor: theme.colors.border,
+    color: theme.colors.primary_text,
+  });
   return (
     <View style={style} {...other}>
       {extra ? (
@@ -101,6 +124,12 @@ export interface ContainerProps extends ScrollViewProps {
 
 export const Container = (props: ContainerProps) => {
   const {children, ...others} = props;
+  const theme = useTheme<Theme>();
+  const styles = createStyles({
+    backgroundColor: theme.colors.background,
+    shadowColor: theme.colors.border,
+    color: theme.colors.primary_text,
+  });
   return (
     <SafeAreaView style={{backgroundColor: '#ededed'}} {...others}>
       <ScrollView style={{height: '100%'}} {...others}>
@@ -119,82 +148,85 @@ export default class Layout extends PureComponent<LayoutProps> {
   static Card = Card;
   render() {
     const {children, style} = this.props;
+    const styles = createStyles({});
     return <View style={[styles.container, style]}>{children}</View>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ededed',
-    minHeight: '100%',
-    flexDirection: 'column',
-  },
-  header: {
-    paddingTop: 25,
-    paddingBottom: 8,
-    paddingLeft: 16,
-    paddingRight: 16,
-    shadowColor: '#E6E6E6',
-    backgroundColor: 'transparent',
-    shadowOffset: {width: 0, height: 3},
-    shadowRadius: 3,
-    // shadowOpacity: 0.8,
-    shadowOpacity: 0,
-    zIndex: 999,
-  },
-  footer: {
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: '#F7F7F7',
-    shadowColor: '#E6E6E6',
-    // shadowOffset: { width: 0, height: -3 },
-    // shadowRadius: 3,
-    // shadowOpacity: 0.8,
-    zIndex: 999,
-  },
-  logo: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 20,
-    color: '#414141',
-  },
-  description: {
-    fontSize: 14,
-    marginTop: 20,
-    color: '#9A9A9A',
-  },
-  copyright: {
-    fontSize: 14,
-    color: '#686868',
-  },
-  cardTitle: {
-    fontSize: 14,
-    backgroundColor: '#F7F7F7',
-    paddingLeft: 16,
-    marginTop: 16,
-    paddingRight: 16,
-    paddingTop: 12,
-    paddingBottom: 12,
-    marginBottom: 16,
-    color: '#9A9A9A',
-  },
-  cardBody: {
-    // paddingLeft: 16,
-    // paddingRight: 16,
-    // paddingBottom: 16,
-  },
-  extra: {
-    backgroundColor: '#F7F7F7',
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-});
+function createStyles({backgroundColor = '#F7F7F7', shadowColor = '#E6E6E6', color = '#9A9A9A'}) {
+  return StyleSheet.create({
+    container: {
+      backgroundColor: backgroundColor,
+      minHeight: '100%',
+      flexDirection: 'column',
+    },
+    header: {
+      paddingTop: 25,
+      paddingBottom: 8,
+      paddingLeft: 16,
+      paddingRight: 16,
+      shadowColor: shadowColor,
+      backgroundColor: backgroundColor,
+      shadowOffset: {width: 0, height: 3},
+      shadowRadius: 3,
+      // shadowOpacity: 0.8,
+      shadowOpacity: 0,
+      zIndex: 999,
+    },
+    footer: {
+      height: 60,
+      justifyContent: 'center',
+      alignItems: 'center',
+      shadowColor: shadowColor,
+      zIndex: 999,
+      backgroundColor: backgroundColor,
+    },
+    body: {
+      backgroundColor: backgroundColor,
+    },
+    logo: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginBottom: 20,
+    },
+    title: {
+      fontSize: 20,
+      color: color,
+    },
+    description: {
+      fontSize: 14,
+      marginTop: 20,
+      color: color,
+    },
+    copyright: {
+      fontSize: 14,
+      color: color,
+    },
+    cardTitle: {
+      fontSize: 14,
+      backgroundColor: shadowColor,
+      paddingLeft: 16,
+      marginTop: 16,
+      paddingRight: 16,
+      paddingTop: 12,
+      paddingBottom: 12,
+      marginBottom: 16,
+      color: color,
+    },
+    cardBody: {
+      // paddingLeft: 16,
+      // paddingRight: 16,
+      // paddingBottom: 16,
+    },
+    extra: {
+      backgroundColor: backgroundColor,
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+  });
+}
 
 Layout.Header = Header;
 Layout.Body = Body;
