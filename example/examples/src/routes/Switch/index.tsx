865f2f8caf4fee84c490b5a3c848e3457a695693
panbibi <1265629139@qq.com>
2023-04-10 14:16:45 +0800

    fix(Switch):解决switch初始值(默认值)不能正确显示状态和添加明暗主题适配

--- example/examples/src/routes/Switch/index.tsx
+++ example/examples/src/routes/Switch/index.tsx
@@ -10,10 +10,7 @@ interface SwitchViewState {
   checked: boolean;
 }
 
-export default class SwitchView extends React.Component<
-  SwitchViewProps,
-  SwitchViewState
-> {
+export default class SwitchView extends React.Component<SwitchViewProps, SwitchViewState> {
   constructor(props: SwitchViewProps) {
     super(props);
     this.state = {
@@ -32,92 +29,45 @@ export default class SwitchView extends React.Component<
         <Layout>
           <Header title={title} description={description} />
           <Body>
-            <Card
-              title={`实例展示，value=${this.state.checked}`}
-              bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
+            <Card title={`实例展示，value=${this.state.checked}`} bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
               <WingBlank>
-                <Flex direction="row">
-                  <Switch
-                    checked={this.state.checked}
-                    onValueChange={this.handleChange}
-                  />
+                <Flex direction="row" style={{marginLeft: 15}}>
+                  <Switch checked={this.state.checked} onValueChange={this.handleChange} />
                   <Spacing type="horizontal" />
-                  <Switch
-                    checked={this.state.checked}
-                    onValueChange={this.handleChange}
-                  />
+                  <Switch checked={this.state.checked} onValueChange={this.handleChange} />
                 </Flex>
               </WingBlank>
             </Card>
-            <Card
-              title={'禁用无法切换，disabled={true}'}
-              bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
+            <Card title={'禁用无法切换，disabled={true}'} bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
               <WingBlank>
-                <Flex direction="row">
-                  <Switch
-                    checked={this.state.checked}
-                    onValueChange={this.handleChange}
-                    disabled
-                  />
+                <Flex direction="row" style={{marginLeft: 15}}>
+                  <Switch checked={this.state.checked} onValueChange={this.handleChange} disabled />
                   <Spacing type="horizontal" />
-                  <Switch
-                    checked={this.state.checked}
-                    onValueChange={this.handleChange}
-                    disabled
-                  />
+                  <Switch checked={this.state.checked} onValueChange={this.handleChange} disabled />
                 </Flex>
               </WingBlank>
             </Card>
-            <Card
-              title={'列表中展示，不受控组件'}
-              bodyStyle={{paddingLeft: 0, paddingRight: 0}}
-            />
-            <List
-              flat={false}
-              extra={<Switch style={{position: 'absolute', right: 0}} />}>
-              <List.Item
-                extra={
-                  <Switch
-                    color="red"
-                    checked
-                    style={{position: 'absolute', right: 0}}
-                  />
-                }>
-                自动设置
-              </List.Item>
-              <List.Item
-                extra={
-                  <Switch
-                    color="#2EA3F4"
-                    checked
-                    style={{position: 'absolute', right: 0}}
-                  />
-                }>
-                24小时制
-              </List.Item>
+            <Card title={'列表中展示，不受控组件'} bodyStyle={{paddingLeft: 0, paddingRight: 0}} />
+            <List flat={false} extra={<Switch style={{position: 'absolute', right: 0}} />}>
+              <List.Item extra={<Switch color="red" checked style={{position: 'absolute', right: 0}} />}>自动设置</List.Item>
+              <List.Item extra={<Switch color="#2EA3F4" checked style={{position: 'absolute', right: 0}} />}>24小时制</List.Item>
               <List.Item>12小时制</List.Item>
             </List>
             <Spacing />
-            <Card
-              title={'设置颜色，color|trackStyle|thumbColor'}
-              bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
+            <Card title={'设置颜色，color|trackStyle|thumbColor'} bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
               <WingBlank>
-                <Flex direction="row">
+                <Flex direction="row" style={{marginLeft: 15}}>
                   <Switch thumbColor="#ffe9a6" />
                   <Spacing type="horizontal" />
-                  <Switch
-                    trackStyle={{backgroundColor: 'red', borderColor: 'red'}}
-                  />
+                  <Switch trackStyle={{backgroundColor: 'red', borderColor: 'red'}} />
                   <Spacing type="horizontal" />
                   <Switch checked={true} color="#393E48" />
                 </Flex>
               </WingBlank>
             </Card>
-            <Card
-              title={"尺寸大小，size?: 'small' | 'default' | 'large';"}
-              bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
+            <Card title={"尺寸大小，size?: 'small' | 'default' | 'large';"} bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
               <WingBlank>
-                <Flex direction="row">
+                <Flex direction="row" style={{marginLeft: 15}}>
                   <Switch size="small" />
                   <Spacing type="horizontal" />
                   <Switch />
@@ -126,11 +76,9 @@ export default class SwitchView extends React.Component<
                 </Flex>
               </WingBlank>
             </Card>
-            <Card
-              title={'尺寸大小，通过设置高宽来定制尺寸'}
-              bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
+            <Card title={'尺寸大小，通过设置高宽来定制尺寸'} bodyStyle={{paddingLeft: 0, paddingRight: 0}}>
               <WingBlank>
-                <Flex direction="row">
+                <Flex direction="row" style={{marginLeft: 15}}>
                   <Switch style={{height: 23, width: 34}} />
                   <Spacing type="horizontal" />
                   <Switch style={{height: 28, width: 42}} />
