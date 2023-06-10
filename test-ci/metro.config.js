596d7189266541b8bcbdfe5388da842dccb4f74a
jaywcjlove <398188662@qq.com>
2023-06-10 11:32:37 +0800

    chore: update scripts.

--- test-ci/metro.config.js
+++ test-ci/metro.config.js
@@ -1,17 +1,2 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+const conf = require('@uimjs/metro-config');
+module.exports = conf.default();
--- website/.kktrc.ts
+++ website/.kktrc.ts
@@ -8,12 +8,14 @@ import pkg from './package.json';
 import { mdCodeModulesLoader } from 'markdown-react-code-preview-loader';
 // @ts-ignore
 import MDS from 'babel-transform-import-plugin';
+
 export default (conf: Configuration, env: 'development' | 'production', options: LoaderConfOptions) => {
   conf = lessModules(conf, env, options);
   // conf = rawModules(conf, env, { ...options });
   conf = scopePluginOptions(conf, env, {
     ...options,
     allowedFiles: [path.resolve(process.cwd(), 'README.md')],
+    allowedPaths: [path.resolve(process.cwd(), '../packages/core')],
   });
 
   conf.module!.exprContextCritical = false;
@@ -35,7 +37,6 @@ export default (conf: Configuration, env: 'development' | 'production', options:
       ],
     ],
   });
-
   conf.resolve = {
     ...(conf.resolve || {}),
     alias: {
@@ -44,6 +45,25 @@ export default (conf: Configuration, env: 'development' | 'production', options:
       '@uiw/react-native': path.join(process.cwd(), './src/react-native'),
     },
   };
+
+  // conf.module!.rules = conf.module?.rules?.map((rules) => {
+  //   if (typeof rules === 'object' && 'oneOf' in rules && rules.oneOf) {
+  //     rules.oneOf = rules.oneOf.map((rule) => {
+  //       if (rule.loader?.includes('babel-loader') && rule.test && rule.test.toString() === /\.(js|mjs|jsx|ts|tsx)$/.toString()) {
+  //         // @ts-ignore
+  //         console.log('rule:', rule);
+  //         rule.include = [
+  //           // path.resolve(process.cwd(), '../packages/core/lib'),
+  //           // path.resolve(process.cwd(), 'node_modules'),
+  //           path.resolve(process.cwd(), 'src')
+  //         ]
+  //       }
+  //       return rule;
+  //     })
+  //   }
+  //   return rules;
+  // });
+
   if (env === 'production') {
     conf.output = { ...conf.output, publicPath: './' };
   }
